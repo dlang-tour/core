55b8a975506da4518a61159128dfa673e540cf59
André Stein <andre.stein.1985@gmail.com>
2016-06-17 21:46:25 +0200

    Revert "Make sure to override default for bind addresses in config when" Revert "Make configuration file optional to make offline starting easier." This reverts commits 6b090c58c5bf0c42844ccd260242791b7ff45a2c and f608d232554ca519e9624db61f401ece05295b80.
    Both commits didn't improve anything for dub offline usage.

--- source/config.d
+++ source/config.d
@@ -1,16 +1,15 @@
 import yaml;
-import vibe.d;
 
 /++
 	Maps values of config.yml to properties.
 +/
 class Config
 {
-	private ushort port_ = 8080;
-	private string[] bindAddresses_ = [ "127.0.0.1" ];
-	private string execProvider_ = "stupidlocal";
+	private ushort port_;
+	private string[] bindAddresses_;
+	private string execProvider_;
 	private bool enableExecCache_;
-	private string publicDir_ = "public";
+	private string publicDir_;
 	private string googleAnalyticsId_;
 
 	private struct DockerConfig {
@@ -47,23 +46,17 @@ class Config
 	
 	this(string configFile)
 	{
-		try {
-			auto root = Loader(configFile).load();
-			port_ = root["port"].as!ushort();
-			bindAddresses_ = [];
-			foreach (string address; root["listen"])
-				bindAddresses_ ~= address;
-			execProvider_ = root["exec"]["driver"].as!string();
-			enableExecCache_ = root["exec"]["cache"].as!bool();
-			publicDir_ = root["public_dir"].as!string();
-			googleAnalyticsId_ = root["google_analytics_id"].as!string();
+		auto root = Loader(configFile).load();
+		port_ = root["port"].as!ushort();
+		foreach (string address; root["listen"])
+			bindAddresses_ ~= address;
+		execProvider_ = root["exec"]["driver"].as!string();
+		enableExecCache_ = root["exec"]["cache"].as!bool();
+		publicDir_ = root["public_dir"].as!string();
+		googleAnalyticsId_ = root["google_analytics_id"].as!string();
 
-			if (execProvider_ == "docker") {
-				dockerConfig_ = DockerConfig(root["exec"]["config"]);
-			}
-		} catch (Exception e) {
-			logError("Error loading config file '%s'. Falling back to defaults: %s",
-				configFile, e);
+		if (execProvider_ == "docker") {
+			dockerConfig_ = DockerConfig(root["exec"]["config"]);
 		}
 	}
 }
