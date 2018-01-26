d3b7682d2b5c77bc633148cd453971a4d6d71f49
Sebastian Wilzbach <seb@wilzba.ch>
2018-01-26 12:04:50 +0100

    Document the daily cron job

---
+++ deploy/docker_update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# remove old, untagged docker images
+docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
+# remove old containers
+docker rm $(docker ps -q -f status=exited)
+# update existing images
+docker images | grep -v REPOSITORY | awk '{print $1}' | xargs -L1 docker pull
