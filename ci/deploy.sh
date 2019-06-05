7612475a81497df633bdcd0890aa8827e56b267b
Geod24 <pro.mathias.lang@gmail.com>
2019-06-05 12:12:44 +0900

    Do not use upstream name while testing
    This avoids Docker pulling the image from the repository if it failed to build locally or was removed.

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,6 +5,7 @@ set -o errexit
 
 if [[ "${TOUR_DEPLOY}" == "1" && "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
     docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+    docker tag dlangtour_test_image dlangtour/core
     docker push dlangtour/core
 fi
 
