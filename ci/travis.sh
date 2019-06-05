d0b47bf2a12ed5b4a7db9caf08a671151920f628
Geod24 <pro.mathias.lang@gmail.com>
2019-06-05 12:12:44 +0900

    Do not use upstream name while testing
    This avoids Docker pulling the image from the repository if it failed to build locally or was removed.

--- ci/travis.sh
+++ ci/travis.sh
@@ -20,6 +20,6 @@ dub --compiler=${DC} -- --sanitycheck
 # Compile to static binary with ldc
 if [[ "${DC}" == "ldc2" ]]; then
     dub build -c static --compiler=${DC}
-    docker build -t dlangtour/core ./
-    docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -ti dlangtour/core --wait-until-pulled --sanitycheck
+    docker build -t dlangtour_test_image ./
+    docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -ti dlangtour_test_image --wait-until-pulled --sanitycheck
 fi
