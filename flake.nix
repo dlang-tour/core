5c9175185c618c0a423663d55da3f259c44bfd9b
Petar Kirov <petar.p.kirov@gmail.com>
2022-03-30 12:29:14 +0300

    build(nix): Add Nix-based dev environment configuration

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Dlang Tour";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.simpleFlake {
+      inherit self nixpkgs;
+      name = "dlang-tour";
+      shell = ./shell.nix;
+    };
+}
